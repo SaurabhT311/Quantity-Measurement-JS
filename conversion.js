4dd991b9932285c13c2803259d4f743b3b6a39e7
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2021-02-12 00:05:59 +0530

    [Saurabh]Add QuantityMeasurementOfLengthVolumeAndTemparature

--- conversion.js
+++ conversion.js
@@ -1,4 +1,4 @@
-// var Promise = require('promise');
+
 class Conversion {
 
     mToKm(data) {
@@ -10,9 +10,8 @@ class Conversion {
                 reject("error is there");
             }
         });
-        // return data*0.001;
+        
     }
-
 
     mToFt(data) {
         return (data / 0.3048);
@@ -50,10 +49,58 @@ class Conversion {
     }
 
     mlToLitres(data) {
-        return data * 0.001;
+        return new Promise((resolve, reject) => {
+            if (data > 0) {
+                let ans = data * 0.001;
+                resolve(ans + " Lt");
+            } else {
+                reject("error is there");
+            }
+        });
+        
     }
+    
     mlToGallons(data) {
         return data / 3785.41;
+    }
+    
+   cToKelvin(data)
+    {
+         return new Promise((resolve, reject) => {
+            if (data > 0) {
+                let kel=273.15;
+                let ans = ((data/1)+kel);
+                 resolve(ans + " K");
+                console.log(ans);
+             } else {
+                reject("error is there");
+            }
+        });
+    }
+
+    cToFahrenheit(data){
+        let answer=(5 * (data + 32))/9;
+        return answer;
+    }
+
+    kToCelcius(data)
+    {
+        return data - 273.15;
+    }
+    kToFahrenheit(data)
+    {
+        let answer=((5*(data - 32 ))/9 + 273.15);
+        return answer;
+    }
+    fToCelcius(data)
+    {
+        let answer=(5 * (data - 32))/9;
+        return answer;
+    }
+    fToKelvin(data)
+    {
+        let answer=((5*(data + 32))/9 + 273.15);
+        return answer;
     }
 }
 module.exports = new Conversion();
